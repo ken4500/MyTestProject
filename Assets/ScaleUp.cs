3f814a487a023429e123bb6b09eac817e565afee
Ken Watanabe <ken.watanabe@gree.net>
2016-06-23 14:12:30 -0700

    first commit

---
+++ Assets/ScaleUp.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ScaleUp : MonoBehaviour {
+
+	public float scaleUpMagnitude;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			var moveHash = new Hashtable();
+			moveHash.Add ("y", scaleUpMagnitude);
+			moveHash.Add ("time", 1f);
+			moveHash.Add ("easeType", "easeInOutSine");
+			iTween.MoveBy (this.gameObject, moveHash);
+		}
+	}
+}
